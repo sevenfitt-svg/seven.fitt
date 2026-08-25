d68923f1dbcd0955dc7477d18ac7ef5d21b9b2c6
sevenfitt-svg <afshin.fire8888@gmail.com>
2026-08-25 11:01:20 +0330

    Update users.service.ts

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,1 +1,13 @@
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
+export class UsersService {
+
+  create(data: any) {
+    return {
+      message: 'User creation service ready',
+      data
+    };
+  }
+
+}
