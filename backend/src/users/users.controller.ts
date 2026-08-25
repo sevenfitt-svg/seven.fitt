633aad9963db023bdda85fd120d2e50dbce000b2
sevenfitt-svg <afshin.fire8888@gmail.com>
2026-08-25 11:04:49 +0330

    Update users.controller.ts

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,1 +1,16 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
 
+@Controller('users')
+export class UsersController {
+
+  constructor(
+    private usersService: UsersService
+  ) {}
+
+  @Post()
+  createUser(@Body() data: any) {
+    return this.usersService.create(data);
+  }
+
+}
